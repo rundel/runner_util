76ebae8d97edfa5dfc98f74bb6d584df3e42313b
Colin Rundel <rundel@gmail.com>
2023-08-31 14:39:33 -0400

    Update create_folders.sh

--- create_folders.sh
+++ create_folders.sh
@@ -3,8 +3,8 @@
 START=1
 END=4
 
-VERSION=2.299.1
-SHA=147c14700c6cb997421b9a239c012197f11ea9854cd901ee88ead6fe73a72c74
+VERSION=2.308.0
+SHA=9f994158d49c5af39f57a65bf1438cbae4968aec1e4fec132dd7992ad57c74fa
 
 curl -o actions-runner-linux-x64.tar.gz -L https://github.com/actions/runner/releases/download/v$VERSION/actions-runner-linux-x64-$VERSION.tar.gz
 
