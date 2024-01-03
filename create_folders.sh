1c9e9e8d031a1456572dcd64afecd8a42ae83ef2
Rapid User <rapiduser@rapid-1733.vm.duke.edu>
2024-01-03 15:17:35 -0500

    Update version

--- create_folders.sh
+++ create_folders.sh
@@ -3,8 +3,8 @@
 START=1
 END=4
 
-VERSION=2.308.0
-SHA=9f994158d49c5af39f57a65bf1438cbae4968aec1e4fec132dd7992ad57c74fa
+VERSION=2.311.0
+SHA=29fc8cf2dab4c195bb147384e7e2c94cfd4d4022c793b346a6175435265aa278
 
 curl -o actions-runner-linux-x64.tar.gz -L https://github.com/actions/runner/releases/download/v$VERSION/actions-runner-linux-x64-$VERSION.tar.gz
 
